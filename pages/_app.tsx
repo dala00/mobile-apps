bbcc779e118e0a4a9c086450be98ff028541f618
Shoichi Namba <alphabrend@gmail.com>
2020-12-15 15:03:08 +0900

    Analytics追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,8 @@
 import '../styles/globals.scss'
+import Router from 'next/router'
+import * as gtag from '../lib/gtag'
+
+Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
